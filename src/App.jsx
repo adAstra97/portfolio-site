1b07bdecb87fdc68a2e6404a903a80b14bad8d82
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2023-10-08 23:01:44 +0300

    add pages and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
 import Layout from './components/Layout/Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layout/>} />
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path='about' element={<About/>} />
+        </Route>
       </Routes>
     </>
   )
---
+++ src/components/About/About.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import './About.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCss3, faGit, faGitAlt, faGithub, faGithubSquare, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+
+const About = () => {
+   const [letterClass, setLetterClass] = useState('text-animate');
+
+   useEffect(() => {
+      setTimeout(() => {
+         return setLetterClass('text-animate-hover');
+      }, 3000);
+   }, []);
+
+   return (
+      <>
+         <div className='container about-page'>
+            <div className="text-zone">
+               <h1>
+                  <AnimatedLetters letterClass={letterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15}/>
+               </h1>
+               <p>
+                  Hello! My name is Veronika, and I am a beginner frontend developer. I'm passionate about creating interactive and functional user interfaces for web applications.
+               </p>
+               <p>
+                  My goal is to become an experienced professional in frontend development and contribute to the creation of user-friendly and efficient online tools.
+               </p>
+               <p>
+                  I have skills in HTML, CSS, and JavaScript, which are the foundation of web development. I am familiar with various frameworks and libraries like React JS and Phaser JS, and I aim to deep dive into learning them.
+               </p>
+            </div>
+
+            <div className='stage-cube-cont'>
+               <div className="cubespinner">
+                  <div className="face1">
+                     <FontAwesomeIcon icon={faHtml5} color='#F06529'/>
+                  </div>
+                  <div className="face2">
+                     <FontAwesomeIcon icon={faReact} color='#28A4D9'/>
+                  </div>
+                  <div className="face3">
+                     <FontAwesomeIcon icon={faJs} color='#EFD81D'/>
+                  </div>
+                  <div className="face4">
+                     <FontAwesomeIcon icon={faGitAlt} color='#EC4D28'/>
+                  </div>
+                  <div className="face5">
+                     <FontAwesomeIcon icon={faCss3} color='#5ED4F4'/>
+                  </div>
+                  <div className="face6">
+                     <FontAwesomeIcon icon={faGithubSquare}  color='#000000'/>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <Loader type='pacman'/>
+      </>
+   )
+};
+
+export default About;
