8bd4f832fb842d78d80fe0602baf90117d5dae14
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2023-10-10 01:20:22 +0300

    add slider

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,14 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { projects } from './data';
 import './Projects.scss';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 
@@ -17,6 +27,39 @@ const Projects = () => {
             <h1>
                <AnimatedLetters strArray={['M', 'y', ' ', 'p', 'r', 'o', 'j', 'e ', 'c', 't', 's']} letterClass={letterClass} idx={11} />
             </h1>
+         </div>
+         <div className="projects">
+            <Swiper
+               effect={'coverflow'}
+               grabCursor={true}
+               centeredSlides={true}
+               slidesPerView={'auto'}
+               coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 60,
+                  modifier: 1,
+                  slideShadows: true,
+               }}
+               pagination={{
+                  type: 'progressbar',
+               }}
+               navigation={true}
+               modules={[EffectCoverflow, Pagination, Navigation]}
+            >
+               {projects.map(project => {
+                  return (
+                     <SwiperSlide className='project' key={project.id}>
+                        <img src={project.image} alt="image" />
+                        <div className='project__info'>
+                           <span className='project__title'>{project.title}</span>
+                           <span className='project__topics'>{project.topics}</span>
+                           <a href={project.link} target='_blank' className='project__link'>Open the project</a>
+                        </div>
+                     </SwiperSlide>
+                  )
+               })}
+            </Swiper>
          </div>
       </div>
    )
